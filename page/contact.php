3a9c1fa3854388bbe16292978bada39361c7489e
marc lambert agas <marclambertagas@gmail.com>
2012-07-27 19:27:09 +0800

    fixes contact page

--- page/contact.php
+++ page/contact.php
@@ -4,10 +4,8 @@
 
     <!-- Header + subtitle -->
     <h1>Contact US</h1>
-    <p class="subtitle-paragraph">
-        <span class="bold">Responsible Adults with phones and voicemail </span>
-    </p>
 <!--    <p class="subtitle-paragraph">So go on, get in touch.. We work quite hard and luckily we LOVE it!</p>-->
+    <p class="subtitle-paragraph"><span class="bold">Responsible Adults with phones and voicemail </span></p>
     <p class="subtitle-paragraph">Contacts are a safe and socially acceptable alternative to glasses. Speaking of which, here are some people you might want to talk to.</p>
     <!--/ Header + subtitle -->
 
@@ -15,30 +13,31 @@
 <!--    <h3>Office Details</h3>-->
     <div class="clear-fix contact-details">
 
-        <div class="contact-details-about">
+        <div id="contact-details" class="contact-details-about">
             <p>
-                Visual Posters<br />
+                <span id="visual-poster" class="bold">Visual Poster</span><br />
                 1300 662 502<br />
-                20A Janette Street, Traralgon, 3844
+                <i id="location"></i><span id="address">20A Janette Street, Traralgon, 3844</span>
             </p>
-
-            <p class="sales-general-inq">
-                Sales & General Inquiries
-            </p>
-            <ul class="no-list">
-                <li class="contact-person"><b>Mike T</b></li>
-                <li class="icon-2 icon-2-1 contact-number">0351759101</li>
-                <li class="contact-person"><b>Christopher C</b></li>
-                <li class="icon-2 icon-2-1 contact-number">0351759105  </li>
-                <li class="icon-2 icon-2-3 contact-number"><a href="mailto:info@visualposters.com.au?Subject=Hello" style="color: #fff;">info@visualposters.com.au</a></li>
-            </ul>
 <!--            <ul class="no-list">
                 <li class="icon-2 icon-2-1">1300 662 502</li>
                 <li class="icon-2 icon-2-3">E: <a href="mailto:info@visualposters.com.au?Subject=Hello" style="color: #fff;">info@visualposters.com.au</a></li>
             </ul>-->
 
         </div>
-
+        <div id="sales-inq">
+            <div class="sales-inq-arrow"></div>
+            <p id="sales-general-inq">
+                Sales & General Inquiries
+            </p>
+            <ul class="no-list">
+                <li class="contact-person">Mike T</li>
+                <li class="contact-number"><i></i>0351759101</li>
+                <li class="contact-person">Christopher C</li>
+                <li class="contact-number"><i></i>0351759105</li>
+                <li class="contact-email"><i></i><a href="mailto:info@visualposters.com.au?Subject=Hello" target="_blank">info@visualposters.com.au</a></li>
+            </ul>
+        </div>
 <!--        <div class="contact-details-map">
             <div class="contact-details-map-arrow"></div>
             <iframe width="245" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=janette+street+traralgon&amp;aq=&amp;sll=-38.204667,146.537029&amp;sspn=0.002331,0.004823&amp;doflg=ptk&amp;ie=UTF8&amp;hq=&amp;hnear=Janette+St,+Traralgon+Victoria+3844,+Australia&amp;t=m&amp;ll=-38.204229,146.537704&amp;spn=0.006745,0.010471&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe><br />
@@ -46,10 +45,10 @@
 
     </div>
     <p>
-        Feel free to contact us from <b>Monday - Friday , 8AM to 9PM weeknights</b>. Meeting exciting people, starting new projects and concepts is what makes us tick. So go on, get in touch.. We work quite hard and luckily we LOVE it!
+        Meeting exciting people, starting new projects and concepts is what makes us tick. So go on, get in touch.. We work quite hard and luckily we LOVE it!
     </p>
 
-    <h3>DROP A LINE</h3>
+    <h3>EMAIL FORM</h3>
     <p>
         One quick favor to make our conversation more productive, let us know what event you're working on and a phone number so we can give you great advice.
     </p>
